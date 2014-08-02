b6d9745bd606c9636762c6b30612b35f60df2ed2
System Administrator <root@zachs-air.attlocal.net>
2014-08-02 18:34:27 -0500

    Updated

--- scripts/gcc.sh
+++ scripts/gcc.sh
@@ -7,13 +7,16 @@ else
 rm -r /gcc-build
 mkdir /gcc-build
 cd /gcc-build
+fi
+echo -n "Input gcc version:";
+read GCCVERSION;
 echo "Downloading GCC";
-wget ftp://ftp.gnu.org/gnu/gcc/gcc-4.8.1/gcc-4.8.1.tar.gz
-tar xvf gcc-4.8.1.tar.gz
+wget ftp://ftp.gnu.org/gnu/gcc/gcc-$GCCVERSION/gcc-$GCCVERSION.tar.gz
+tar xvf gcc-$GCCVERSION.tar.gz
 mkdir gcc-build
 cd gcc-build
 echo "Configureing GCC"
-../gcc-4.8.1/configure --prefix=/usr/local --enable-languages=c,c++,objc
+../gcc-$GCCVERSION/configure --prefix=/usr/local --enable-languages=c,c++,objc
 echo "Building GCC"
 make
 echo "Installing GCC"
